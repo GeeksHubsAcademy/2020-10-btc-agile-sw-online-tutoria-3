9a1896ca2d4ffaf6eb0680d616dd6646b25f3bc5
v.bolinches <v.bolinches@geekshubs.com>
2020-11-05 18:05:24 +0100

    copia de repositorio

---
+++ src/es/geekshubs/kata1/hello.ts
@@ -0,0 +1,3 @@
+export function getHello(param:string) : string {
+    return "Hello "+param;
+}
---
+++ src/es/geekshubs/kata1/main.ts
@@ -0,0 +1,12 @@
+import { getHello } from "../kata1/hello"
+
+function main() {
+    print()
+}
+
+function print(){
+    var res = getHello("vicboma1")
+    console.log(res)
+}
+
+main()
